b1ffbbf8c3f4b916c216cb11a26eb0afee8c2ef6
carlynorama <carlynorama@gmail.com>
2017-05-18 11:57:00 -0700

    Non-animated example. See https://gist.github.com/carlynorama/ac3da1a64fb36842ac911efbb14d6fdc

---
+++ natcode_00_00_CALayer101/AnimationView.swift
@@ -0,0 +1,35 @@
+//
+//  AnimationView.swift
+//  natcode_00_00_CALayer101
+//
+//  Created by Carlyn Maw on 5/14/17.
+//  Copyright © 2017 carlynorama. All rights reserved.
+//
+
+import UIKit
+
+
+@IBDesignable
+class AnimationView: UIView {
+    
+    
+    func setupLayer() {
+        
+        //the backgroundcolor property gets set during viewWillLoad/viewDidLoad?
+        //apparently can't be updated after?
+        let easyChangeBackgorund = CAGradientLayer()
+        easyChangeBackgorund.frame = bounds
+        easyChangeBackgorund.colors = [UIColor.blue.cgColor, UIColor.black.cgColor]
+        layer.insertSublayer(easyChangeBackgorund, at: 0)
+        
+        layer.borderWidth = 100.0
+        layer.borderColor = UIColor.red.cgColor
+        layer.shadowOpacity = 0.7
+        layer.shadowRadius = 10.0
+    }
+    
+    override func draw(_ rect: CGRect) {
+        setupLayer()
+    }
+    
+}
--- natcode_00_00_CALayer101/Sketch.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Sketch.swift
-//  natcode_00_00_CALayer101
-//
-//
-//
-
